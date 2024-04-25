2d6e3699e7c5ce896b9b359fb6f27b40b439fe90
Brooke Joseph <114883803+brookejoseph@users.noreply.github.com>
2024-04-25 09:54:12 -0700

    Create imagesegmentation.py

---
+++ imagesegmentation.py
@@ -0,0 +1,67 @@
+import ultralytics
+from ultralytics import YOLO
+from segment_anything import sam_model_registry, SamAutomaticMaskGenerator, SamPredictor
+from IPython.display import display, Image
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+ultralytics.checks()
+
+!yolo predict model=yolov8n.pt source='/content/IMG_1617.JPG'
+
+Image(filename='/content/runs/detect/predict/IMG_1617.JPG', height=600)
+
+model = YOLO('/content/yolov8n.pt')
+results = model.predict(source='/content/IMG_1617.JPG', conf=0.25)
+for result in results:
+    boxes = result.boxes
+
+bbox=boxes.xyxy.tolist()[0]
+
+bbox
+
+sam_checkpoint = "/content/sam_vit_h_4b8939.pth"
+model_type = "vit_h"
+sam = sam_model_registry[model_type](checkpoint=sam_checkpoint)
+predictor = SamPredictor(sam)
+
+image = cv2.cvtColor(cv2.imread('/content/IMG_1617.JPG'), cv2.COLOR_BGR2RGB)
+predictor.set_image(image)
+
+def show_mask(mask, ax, random_color=False):
+    if random_color:
+        color = np.concatenate([np.random.random(3), np.array([0.6])], axis=0)
+    else:
+        color = np.array([30/255, 144/255, 255/255, 0.6])
+    h, w = mask.shape[-2:]
+    mask_image = mask.reshape(h, w, 1) * color.reshape(1, 1, -1)
+    ax.imshow(mask_image)
+
+def show_points(coords, labels, ax, marker_size=375):
+    pos_points = coords[labels==1]
+    neg_points = coords[labels==0]
+    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+
+def show_box(box, ax):
+    x0, y0 = box[0], box[1]
+    w, h = box[2] - box[0], box[3] - box[1]
+    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0,0,0,0), lw=2))
+
+
+input_box = np.array(bbox)
+
+masks, _, _ = predictor.predict(
+    point_coords=None,
+    point_labels=None,
+    box=input_box[None, :],
+    multimask_output=False,
+)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(image)
+show_mask(masks[0], plt.gca())
+show_box(input_box, plt.gca())
+plt.axis('off')
+plt.show()
+
